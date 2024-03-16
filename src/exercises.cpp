e40c064dde45ccb00a68c406b201d7089e744b7f
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-03-15 21:04:43 -0400

    enviando cambios ejercicio 1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -26,8 +26,6 @@ void exercise_1(string s1, string s2, string s3) {
 
 void exercise_2(double A, double B, double C) {
   //your code here
-   double discriminante = B * B - 4 * A * C;
-
     if (discriminante >= 0) {
         double raiz1 = (-B + sqrt(discriminante)) / (2 * A);
         double raiz2 = (-B - sqrt(discriminante)) / (2 * A);
