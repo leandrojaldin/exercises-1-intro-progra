af1bf86f9d914e40c702614c585fc2551c93ccf6
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-03-15 21:59:44 -0400

    corregido 2

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -17,7 +17,7 @@ void exercise_1(string s1, string s2, string s3) {
 
 
 void exercise_2(double A, double B, double C) {
-    double D = B * B - 4 A * C ;
+    double D = B * B - 4 * A * C ;
     if (D > 0 && A != 0)
     {
          double d_sqrt = sqrt (D);
@@ -41,7 +41,7 @@ void exercise_2(double A, double B, double C) {
     }
     else
     {
-          cout<<"":
+          cout<<"";
     }
 
 }
