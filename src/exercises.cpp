8f374316a80677840a762c91da310466e8d861d1
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-03-16 10:27:42 -0400

    envio 9

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -148,7 +148,19 @@ void exercise_8(long int seconds) {
 
 string exercise_9 (string s1, string s2, string s3, string s4, string s5) {
   // TODO: YOUR CODE HERE
-  return"";
+     string result = "";
+    result += s1[0];
+    result += s2[0];
+    result += s3[0];
+    result += s4[0];
+    result += s5[0];
+
+    // Verificar si el primer caracter de la primera palabra y la última palabra son iguales
+    if (s1[0] == s5[0]) {
+        return result + "\nHemos encontrado algo!";
+    } else {
+        return result + "\nAun sin suerte";
+    }
 }
 
 
@@ -164,11 +176,7 @@ int exercise_10(int a, int b) {
 
 string exercise_11(int number) {
   // TODO: YOUR CODE HERE
-     if (isFibonacci(number)) {
-        return "Se encontro Fibonacci";
-    } else {
-        return "Esto no es de Fibonacci";
-    }
+  return "";
 }
 
 void exercise_12(string color1, int numb1,
