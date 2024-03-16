5f3c29f050000850bfbfef14e9cbf113a6abd70a
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-03-15 22:40:49 -0400

    cambios 10

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -131,14 +131,11 @@ string exercise_9 (string s1, string s2, string s3, string s4, string s5) {
 
 int exercise_10(int a, int b) {
   // TODO: YOUR CODE HERE
-   if (a < 0 || b < 0) {
+     if (a < 0 || b < 0)
         return 0;
-    }
-
     int sum = a + b;
-    int sum_digits = log10(sum) + 1;
-
-    return sum * sum_digits;
+    int digit_count = (sum == 0) ? 1 : (int)log10(abs(sum)) + 1;
+    return sum * digit_count;
 }
 
 string exercise_11(int number) {
