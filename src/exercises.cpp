06908e9bb50cb7179b572db0ae3a31df845aca60
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-03-16 00:51:18 -0400

    camb 15

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -187,6 +187,15 @@ string exercise_14(int number_of_docs) {
 
 void exercise_15(int a, int b, int c) {
   // TODO: YOUR CODE HERE
+     cout << "Los valores son: a = " << a << " b = " << b << " y c = " << c << endl;
+    
+    int temp = a;
+    a = b;
+    b = c;
+    c = temp;
+
+    cout << "Permutamos: a => b, b => c, c => a" << endl;
+    cout << "Los valores despues de la permutacion son: a = " << a << " b = " << b << " c = " << c << endl
 }
 
 void exercise_16(int debut, int fin) {
