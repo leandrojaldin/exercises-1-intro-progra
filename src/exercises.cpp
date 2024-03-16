d9d7c74b7b5fbdf9960a061d5176c054d4e9a4f3
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-03-15 23:08:18 -0400

    cambios 9

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -124,6 +124,17 @@ void exercise_8(long int seconds) {
 
 string exercise_9 (string s1, string s2, string s3, string s4, string s5) {
   // TODO: YOUR CODE HERE
+      string result = "";
+    result += s1[0];
+    result += s2[0];
+    result += s3[0];
+    result += s4[0];
+    result += s5[0];
+    if (s1[0] == s5[0]){
+        result "\nHemos encontrado algo!";
+    }else{
+        result "\nAun sin suerte";
+    }
  }
 
 int exercise_10(int a, int b) {
