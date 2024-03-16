af6ca6708ab1707ad45f4d90b831793aa0947285
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-03-16 09:50:45 -0400

    ejercicio 13

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -202,7 +202,24 @@ void exercise_12(string color1, int numb1,
 
 string exercise_13(int age, int years_of_experience) {
   // TODO: YOUR CODE HERE
-  return "";
+  cin>> age;
+  cin >> years_of_experience;
+  if (age>=18 && years_of_experience>=5)
+  {
+    return"Senior project manager";
+  } 
+  else if (age>=18 && years_of_experience>=3)
+  {
+    return"Project manager";
+  } 
+  else if (age>=18)
+  {
+    return"Project coordinator";
+  } 
+  else
+  {
+    return"Not eligible";
+  } 
 }
 
 string exercise_14(int number_of_docs) {
